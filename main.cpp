38cad07d3f25a1ec96420d1991e17298d1634651
Vishnu R <vishnurapps@gmail.com>
2019-01-03 23:28:23 +0530

    modifications for material design

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQuickStyle>
 
 int main(int argc, char *argv[])
 {
@@ -8,6 +9,8 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+
+    QQuickStyle::setStyle("Material");
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     if (engine.rootObjects().isEmpty())
         return -1;
